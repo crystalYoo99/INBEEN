4fb3ccdbf32495a6a3e824215141b74baaa97e22
hibeen1 <21201049@handong.edu>
2019-04-12 11:02:07 +0900

    인빈's main.c

---
+++ main.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+void sub1();
+void sub2();
+void sub3();
+void sub4();
+
+int main(){
+	sub1();
+	sub2();
+	sub3();
+	sub4();
+	return 0;
+}
+
+void sub1(){
+
+}
+
+void sub2(){
+
+}
+
+void sub3(){
+
+}
+
+void sub4(){
+	int i,j;
+	for (i=0; i<10 ; i++){
+		for (j=0; j<i ; j++) printf("*");
+		printf("\n");
+	}
+}
+
